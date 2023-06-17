e54319e62af4135a0665ff3dcf35da0600d07608
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-16 23:32:41 -0400

    Create method to shuffle cards

--- deck.py
+++ deck.py
@@ -1,16 +1,22 @@
 from card import Card
+import random # used to shuffle deck
 
 class Deck:
 
     def __init__(self):
-        self.all_cards = []
+        self.cards = []
 
+        # adds each card to cards array
         for suit in Card.suits:
             for rank in Card.ranks:
 
                 card = Card(rank, suit)
 
-                self.all_cards.append(card)
+                self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
 
 
 
