ceae8f6886202cf5f6976038e124403760622c9d
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-17 19:51:34 -0400

    Add comments to outline logic of a Blackjack hand

--- game.py
+++ game.py
@@ -4,4 +4,31 @@ from deck import Deck
 
 # run code here
 if __name__ == "__main__":
+
     pass
+    
+    # First, create a deck of 52 cards
+    
+    # Shuffle the deck
+
+    # Ask the player for their bet
+
+    # Make sure bet doesn't exceed their chips
+
+    # deal two cards to player and dealer
+
+    # show only one of dealers cards; other is hidden
+    
+    # show both of player's cards
+
+    # ask the player to hit and take another card
+
+    # if they don't bust (go over 21) ask again
+
+    # if player stands, play dealer's hand. Dealer will always hit until their value is >= 17
+
+    # determine winner and adjust chips accordingly
+
+    # ask player to play again
+
+
