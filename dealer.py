8708a833067c32c54e444e0a53575050be70d205
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-17 20:42:49 -0400

    Create string representations for dealer and player hands

--- dealer.py
+++ dealer.py
@@ -2,3 +2,8 @@ class Dealer():
 
     def __init__(self):
         self.hand = []
+
+    def __str__(self):
+        print("Dealer's Hand:")
+        print("\t" + self.hand[0])
+        print("\tCARD HIDDEN")
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from dealer import Dealer
 
+# Player class inherits from Dealer class
 class Player(Dealer):
 
     def __init__(self):
@@ -8,3 +9,8 @@ class Player(Dealer):
     # places a bet
     def place_bet(self, chips):
         self.chips -= chips
+
+    def __str__(self):
+        print("Player's Hand:")
+        print("\t" + self.hand[0])
+        print("\t" + self.hand[1])
