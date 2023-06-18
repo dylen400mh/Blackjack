4e1d573baf6b3a7533136b8df0d7075eb354c8ce
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-17 20:43:15 -0400

    Create for loop to deal two cards to player and dealer

--- game.py
+++ game.py
@@ -1,54 +1,57 @@
 from card import Card
 from deck import Deck
 from player import Player
+from dealer import Dealer
 
 
 # run code here
 if __name__ == "__main__":
 
     pass
-    
+
     # First, create a deck of 52 cards
 
     deck = Deck()
-    
+
     # Shuffle the deck
 
     deck.shuffle()
 
-    # initialize player object
+    # initialize player and dealer objects
 
     player = Player()
+    dealer = Dealer()
 
     print("Welcome to Blackjack! The goal is to get as close to 21 as possible without going over!")
-    print("The dealer will hit until they reach 17. Aces can count as 1 or 11.")
+    print("The dealer will hit until they reach 17. Aces can count as 1 or 11.\n")
 
     # Ask the player for their bet
-    
+
     print(f"You currently have {player.chips} chips.")
 
     while True:
         try:
-          bet = int(input("Place your bet: "))
+            bet = int(input("Place your bet: "))
         except:
             print("Please enter a valid number.")
         else:
+            # Make sure bet doesn't exceed their chips
             if bet > player.chips:
                 print("You have an insufficient number of chips. Please try again.")
             elif bet < 1:
-                print("You must bet at least 1 chip. Please try again.")
+                print("Your must bet at least 1 chip. Please try again.")
             else:
                 player.place_bet(bet)
                 break
-        
-
-
-    # Make sure bet doesn't exceed their chips
 
     # deal two cards to player and dealer
 
+    for _ in range(2):
+        player.hand.append(deck.deal())
+        dealer.hand.append(deck.deal())
+
     # show only one of dealers cards; other is hidden
-    
+
     # show both of player's cards
 
     # ask the player to hit and take another card
@@ -60,5 +63,3 @@ if __name__ == "__main__":
     # determine winner and adjust chips accordingly
 
     # ask player to play again
-
-
