a62206f486205d84b2c7de2afed8f71512482842
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-17 20:26:23 -0400

    Add fields to inside of class

--- card.py
+++ card.py
@@ -1,16 +1,15 @@
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
-          'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
-
-
 class Card:
+
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
+          'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
 
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-        self.value = values[rank]
+        self.value = Card.values[rank]
 
     # returns string representation of card
     def __str__(self):
