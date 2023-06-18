80d190976095c6db5ecb051ae0fd93723bfc8e7c
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-17 20:35:19 -0400

    Have Player class inherit Dealer class

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
-class Player:
+from dealer import Dealer
+
+class Player(Dealer):
 
     def __init__(self):
         self.chips = 100 # player starts with 100 chips
-        self.hand = []
     
     # places a bet
     def place_bet(self, chips):
