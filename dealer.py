096eb62a9b9bda1f8fc5f5d7bec03d1281a5e000
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-18 00:46:57 -0400

    Combine setter and getter methods into one method only

--- dealer.py
+++ dealer.py
@@ -1,19 +1,22 @@
 from card import Card
 
-
 class Dealer():
 
     def __init__(self):
         self.hand = []
         self.hand_value = 0
-
+    
     # determines how many points the user has in their hand
-    def set_hand_value(self):
+    def update_hand_value(self):
 
-        self.hand_value = 0  # reset this and count all cards again
+        self.hand_value = 0 #reset this and count all cards
 
         for card in self.hand:
             self.hand_value += card.value
-
-    def get_hand_value(self):
+        
         return self.hand_value
+
+
+
+    
+
