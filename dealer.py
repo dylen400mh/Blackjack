16576e54732ea19b3a11b87461483a97bad8d3f1
dylen400mh <dylenbelanger2004@gmail.com>
2023-06-17 23:20:58 -0400

    Fix hand_value from accumulating

--- dealer.py
+++ dealer.py
@@ -1,18 +1,19 @@
 from card import Card
 
+
 class Dealer():
 
     def __init__(self):
         self.hand = []
         self.hand_value = 0
-    
+
     # determines how many points the user has in their hand
     def set_hand_value(self):
+
+        self.hand_value = 0  # reset this and count all cards again
+
         for card in self.hand:
             self.hand_value += card.value
 
     def get_hand_value(self):
         return self.hand_value
-
-    
-
